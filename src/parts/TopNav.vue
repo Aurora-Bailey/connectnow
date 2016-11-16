e02d7d823854bfa589b549b437dd8abb031bd05a
Brad Thompson <bradleyglind@gmail.com>
2016-11-16 05:33:07 -0700

    Top navigation done

--- src/parts/TopNav.vue
+++ src/parts/TopNav.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="top-nav">
-    <!-- use router-link component for navigation. -->
-    <!-- specify the link by passing the `to` prop. -->
-    <!-- <router-link> will be rendered as an `<a>` tag by default -->
-    <router-link v-for="link in links" v-bind:class="[link.class]" v-bind:to="link.route">Go to {{link.text}}</router-link>
+
+    <div class="top-nav-fixed">
+      <!-- use router-link component for navigation. -->
+      <!-- specify the link by passing the `to` prop. -->
+      <!-- <router-link> will be rendered as an `<a>` tag by default -->
+      <router-link class="nav-link home" to="/" tag="div"></router-link>
+
+      <div class="mobile-toggle" v-on:click="toggle=!toggle"><i class="fa fa-bars"></i></div>
+      <div class="mobile-contain" :class="{open: toggle}">
+        <router-link class="nav-link search" to="/playing">search</router-link>
+        <router-link class="nav-link matches" to="/matches">matches</router-link>
+        <router-link class="nav-link connections" to="/connections">connections</router-link>
+        <router-link class="nav-link groups" to="/groups">groups</router-link>
+        <router-link class="nav-link profile" to="/profile">profile</router-link>
+        <router-link class="nav-link logout" to="/logout">log out</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,10 +27,7 @@
     name: 'top-nav',
     data () {
       return {
-        links: [
-          {class: 'home', text: 'Home', route: '/'},
-          {class: 'playing', text: 'Playing', route: '/playing'}
-        ]
+        toggle: false
       }
     }
   }
@@ -29,6 +39,85 @@
   @import "../sass/Variables"
   @import "../sass/Mixins"
 
+  $nav-height: 65px
+
   .top-nav
+    height: $nav-height
+
+  .top-nav-fixed
+    position: fixed
+    z-index: 1000
+    top: 0
+    left: 0
+    right: 0
+    box-shadow: 0 2px 8px 0 rgba(50, 50, 50, 0.08)
+    background-color: $base
+
+  .nav-link
+    height: $nav-height
+    line-height: $nav-height
+    padding: 0 15px
+    display: inline-block
+    font-size: 13px
+    font-weight: bold
+    text-transform: uppercase
+    vertical-align: text-top
+
+  .mobile-toggle
+    display: none
+    position: absolute
+    top: 10px
+    right: 20px
+    width: 45px
+    height: 45px
+    text-align: center
+    line-height: 45px
+    font-size: 25px
+    cursor: pointer
+
+  .mobile-contain
+    float: right
+    display: inline-block
+    max-height: $nav-height
+    overflow: hidden
+    padding: 0 20px
+
+  .home
+    margin-left: 20px
+    width: 265px
+    height: $nav-height
+    padding: 0
+    display: inline-block
+    background-image: url('../assets/logo.png')
+    background-repeat: no-repeat
+    background-position: center
+    cursor: pointer
+
+  @media screen and (max-width: 750px)
+    .nav-link
+      height: 55px
+      line-height: 55px
+      display: block
+
+    .home
+      height: $nav-height
+
+    .mobile-toggle
+      display: inline-block
+
+    .mobile-contain
+      float: none
+      display: block
+      max-height: 0
+      -webkit-box-shadow: inset 0 1px 1px 0 darken($base, 10%)
+      -moz-box-shadow: inset 0 1px 1px 0 darken($base, 10%)
+      box-shadow: inset 0 1px 1px 0 darken($base, 10%)
+      -webkit-transition: max-height .5s ease
+      transition: max-height .5s ease
+
+      &.open
+        max-height: 350px
+
+  .this-is-to-make-webstorm-recognize-end-of-sass-line
     opacity: 1
 </style>
