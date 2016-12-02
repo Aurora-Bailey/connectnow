a9fc112861fbf4bd2822e0e577a5aebb4880a933
Brad Thompson <bradleyglind@gmail.com>
2016-12-01 18:25:01 -0700

    Breakout routes into separate file

--- src/main.js
+++ src/main.js
@@ -4,41 +4,21 @@ import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 import App from './App'
 import VueData from './modules/VueData'
+import Routes from './Routes'
 
 // Bootstrap
 import 'jquery'
 import 'tether'
 import 'bootstrap'
 
-// Pages
-import Home from './pages/Home'
-import Search from './pages/Search'
-import Matches from './pages/Matches'
-import Connections from './pages/Connections'
-import Groups from './pages/Groups'
-import Profile from './pages/Profile'
-import Logout from './pages/Logout'
-import NotFound from './pages/NotFound'
-
 // The Vue thing
 Vue.use(VueRouter)
 Vue.use(VueResource)
 
-// Router
-const routes = [
-  { path: '/', component: Home },
-  { path: '/search', component: Search },
-  { path: '/matches', component: Matches },
-  { path: '/connections', component: Connections },
-  { path: '/groups', component: Groups },
-  { path: '/profile', component: Profile },
-  { path: '/logout', component: Logout },
-  { path: '*', component: NotFound }
-]
 const router = new VueRouter({
   // History mode makes links prettier, but requires dedicated hosting. (or in s3 set 404 to index.html)
   mode: 'history',
-  routes // short for routes: routes
+  routes: Routes // short for routes: routes
 })
 
 // Vue Main
