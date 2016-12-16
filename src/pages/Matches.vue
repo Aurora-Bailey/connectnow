6310890827b84c07b1cb63989842f0ee31f4e52f
Brad Thompson <bradleyglind@gmail.com>
2016-12-16 15:24:31 -0700

    Matches basic style

--- src/pages/Matches.vue
+++ src/pages/Matches.vue
@@ -1,62 +1,75 @@
 <template>
   <div id="matches" >
-    <div class="search-by">
-      <div class="title">Search</div>
-
-      <div class="category individual-tag">
-        <div class="text">Individual Tag:</div>
-        <input class="input" type="text">
-      </div>
-
-      <div class="category sex">
-        <div class="text">Sex:</div>
-        <select class="input">
-          <option value="male" selected>Male</option>
-          <option value="female">Female</option>
-          <option value="yes">Yes</option>
-        </select>
-      </div>
-
-      <div class="category proximity">
-        <div class="text">Proximity:</div>
-        <div class="multirange">
-          <input class="input" value="10" type="range">
-          <input class="input" type="range">
+    <div class="card filter">
+      <div class="section head">
+        <div class="search">
+          <i class="material-icons search-icon">search</i>
+          <input class="search-input" type="text" placeholder="Search">
+        </div>
+        <div class="toggle_filter_options" @click="filter_options=!filter_options">
+          <i class="material-icons">settings</i>
         </div>
       </div>
 
-      <div class="category age">
-        <div class="text">Age:</div>
-        <div class="multirange">
-          <input class="input" value="10" type="range">
-          <input class="input" type="range">
+      <div class="section options" :class="{open: filter_options}">
+        <div class="category individual-tag">
+          <div class="text">Individual Tag:</div>
+          <input type="text">
+        </div>
+
+        <div class="category sex">
+          <div class="text">Sex:</div>
+          <select>
+            <option value="male" selected>Male</option>
+            <option value="female">Female</option>
+            <option value="yes">Yes</option>
+          </select>
+        </div>
+
+        <div class="category proximity">
+          <div class="text">Proximity:</div>
+          <div class="multirange">
+            <input value="10" type="range">
+            <input type="range">
+          </div>
+        </div>
+
+        <div class="category age">
+          <div class="text">Age:</div>
+          <div class="multirange">
+            <input value="10" type="range">
+            <input type="range">
+          </div>
+        </div>
+
+        <div class="category experience">
+          <div class="text">Experience:</div>
+          <div class="multirange">
+            <input value="10" type="range">
+            <input type="range">
+          </div>
         </div>
       </div>
-
-      <div class="category experience">
-        <div class="text">Experience:</div>
-        <div class="multirange">
-          <input class="input" value="10" type="range">
-          <input class="input" type="range">
+    </div>
+    <div class="card matches">
+      <div class="section multiple">
+        <div class="title">Multiple Matches</div>
+        <div v-for="match in matches" class="person">
+          <div class="picture" :style="{backgroundImage: 'url(' + match.picture + ')'}"></div>
+          <div class="age">Age: {{match.age}}</div>
+          <div class="location">Location: {{match.location}}</div>
         </div>
       </div>
-
-      <div class="category joke">
-        max-width:
-        <br>
-        150px
+      <div class="section single">
+        <div class="title">Specific Tag</div>
+        <div v-for="match in matches" class="person">
+          <div class="picture" :style="{backgroundImage: 'url(' + match.picture + ')'}"></div>
+          <div class="age">Age: {{match.age}}</div>
+          <div class="location">Location: {{match.location}}</div>
+        </div>
       </div>
 
     </div>
-    <div class="multiple-matches">
-      <div class="title"></div>
-      <div v-for="match in matches" class="card">
-        <div class="info picture" :style="{backgroundImage: 'url(' + match.picture + ')'}"></div>
-        <div class="info age">Age: {{match.age}}</div>
-        <div class="info location">Location: {{match.location}}</div>
-      </div>
-    </div>
-    <div class="preview-person"></div>
   </div>
 </template>
 
@@ -68,7 +81,8 @@
     data () {
       return {
         matches: [],
-        preview: {}
+        preview: {},
+        filter_options: false
       }
     },
     mounted: function () {
@@ -99,60 +113,81 @@
 
   #matches
 
-    .search-by
-      position: fixed
-      top: 65px // for top nav
-      left: 0
-      bottom: 0
-      z-index: 999
-      display: inline-block
-      width: 210px
-      text-align: center
-      box-shadow: 5px 10px 15px 5px rgba(0,0,0,.1);
-      border-top: 1px solid color-level(background, -1)
-      background-color: color(background)
+    .filter .head
+      color: color-text(color(background), secondary)
+      position: relative
 
-      .title
-        margin: 30px 0
-        font-size: 1.3em
-        text-decoration: underline
+      .toggle_filter_options
+        display: inline-block
+        position: absolute
+        top: 0
+        right: 6px
+        bottom: 0
+        margin: auto
+        height: 24px
+        width: 24px
+        font-size: 24px
+        cursor: pointer
+
+      .search
+        position: relative
+        margin-right: 36px
+
+        .search-icon
+          height: 24px
+          width: 24px
+          font-size: 24px
+          position: absolute
+          top: 0
+          left: 12px
+          bottom: 0
+          margin: auto
+
+        .search-input
+          padding-left: 36px
+
+    .filter .options
+      max-height: 0
+      padding: 0 3px
+      overflow: hidden
+      transition: all .5s ease
+      +contain()
+
+      &.open
+        max-height: 400px
 
       .category
-        margin: 20px 0
-        padding: 0 15px
-
-        .text
-          margin-bottom: 10px
-
-        .input
-          width: 100%
-
-    .multiple-matches
-      margin-left: 250px
-      margin-top: 40px
-      width: 550px
-
-      .card
-        background-color: color(background)
+        +span(4, 4)
         padding: 10px
-        margin: 10px 0
-        box-shadow: 0 1px 2px rgba(0,0,0,.1);
-        white-space: nowrap
 
-        .info
-          display: inline-block
-          vertical-align: text-top
-          line-height: 75px
-          margin: 10px
-          text-transform: capitalize
-
-        .picture
-          width: 75px
+        +respond-over(w480)
+          +span(4, 12)
           height: 75px
-          border-radius: 75px
-          background-position: center
-          background-size: cover
-          box-shadow: 0 1px 2px rgba(0,0,0,.1);
+          text-align: center
+
+    .section
+      .title
+        padding: 10px
+        font-weight: bold
+
+    .person
+      white-space: nowrap
+      border-bottom: 1px solid color-text(color(background), divider)
+
+      .age, .location, .picture
+        display: inline-block
+        vertical-align: text-top
+        line-height: 75px
+        margin: 10px
+        text-transform: capitalize
+
+      .picture
+        width: 75px
+        height: 75px
+        border-radius: 75px
+        background-position: center
+        background-size: cover
+        box-shadow: 0 1px 2px rgba(0,0,0,.1);
 
   .asdf
     display: none
